78cefe7bdc944b11034b222f5b94255e08909292
Sergey Kuklin <ksv.crowley@gmail.com>
2022-06-05 22:17:49 +0500

    feat: Adds summary

--- src/features/OccupancyOptimizator/OccupancyOptimizator.tsx
+++ src/features/OccupancyOptimizator/OccupancyOptimizator.tsx
@@ -1,9 +1,12 @@
 import { Form, FormContextProvider } from './components/Form';
+import { Summary } from './components/Summary';
 
 export const OccupancyOptimizator = () => {
   return (
     <FormContextProvider>
+      <h1>Occupancy Optimizator</h1>
       <Form />
+      <Summary />
     </FormContextProvider>
   );
 };
---
+++ src/features/OccupancyOptimizator/components/Summary/Summary.tsx
@@ -0,0 +1,46 @@
+import { Card } from 'shared/components/Card';
+import { useGetSummary } from './useGetSummary';
+
+import styles from './Summary.module.css';
+import { useMemo } from 'react';
+
+type Row = {
+  type: string;
+  total: number;
+  occupiedRooms: number;
+};
+
+export const Summary = () => {
+  const results = useGetSummary();
+
+  const rows: Row[] = useMemo(
+    () =>
+      Object.entries(results).map(([type, { total, occupiedRooms }]) => ({
+        type,
+        total,
+        occupiedRooms
+      })),
+    [results]
+  );
+
+  return (
+    <Card title="Summary">
+      <table className={styles.table}>
+        <thead>
+          <td />
+          <td>Usage</td>
+          <td>Revenue</td>
+        </thead>
+        <tbody>
+          {rows.map(({ type, total, occupiedRooms }) => (
+            <tr key={type}>
+              <td className={styles.type}>{type}:</td>
+              <td>{occupiedRooms}</td>
+              <td>{total} €</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Card>
+  );
+};
